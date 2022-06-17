60791f2685b6d976c757c448f197f2c1ce598123
gary shannon <gary@garyshannon.com.au>
2022-06-18 08:23:39 +1000

    Fixed for new opensea layout

--- static/js/worker.js
+++ static/js/worker.js
@@ -3,17 +3,19 @@ needs_update = false;
 
 function updateToons() {  
   if (needs_update && toons.length > 0) {
-      matches = document.querySelectorAll(".AssetCardFooter--name");
+      matches = document.querySelectorAll(".AssetsSearchView--assets > div > div > div > div > div > article > a > div:nth-child(2) > div:nth-child(1) > div > div")
       matches.forEach(function(el) {
-          els = el.innerHTML.split('TOONZ #');
-          els = els[1].split(' (Claimed)');
-          token_id = els[0];        
-          
-          if (toons.includes(parseInt(token_id))) {
-              el.innerHTML = 'TOONZ #' + parseInt(token_id) + ' (Claimed)';
-              el.classList.add('claimed-toon');
-          } else {
-            el.classList.add('unclaimed-toon');
+          if (el.innerHTML) {
+            els = el.innerHTML.split('TOONZ #');
+            els = els[1].split(' (Claimed)');
+            token_id = els[0];        
+            
+            if (toons.includes(parseInt(token_id))) {
+                el.innerHTML = 'TOONZ #' + parseInt(token_id) + ' (Claimed)';
+                el.classList.add('claimed-toon');
+            } else {
+              el.classList.add('unclaimed-toon');
+            }
           }
       });
 
